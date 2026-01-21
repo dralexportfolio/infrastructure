06eed23c1daf75bbb4651a2c56cc7c25a458e198
dralexportfolio <dr.alex.portfolio@gmail.com>
2026-01-20 17:28:15 -0800

    Update Board.py

--- board_games/Board.py
+++ board_games/Board.py
@@ -27,20 +27,17 @@ from typing import Any
 board_decorator = private_attributes_dec("_all_bevel_info_flag",		# class variables
 										 "_all_deepcopy_polygons",
 										 "_all_sun_info_flag",
-										 "_bevel_attitude",
-										 "_bevel_size",
 										 "_n_polygons",
 										 "_render_axis",
 										 "_render_figure",
-										 "_sun_angle",
-										 "_sun_attitude",
 										 "_x_lower",
 										 "_x_shift_per_polygon",
 										 "_x_upper",
 										 "_y_lower",
 										 "_y_shift_per_polygon",
 										 "_y_upper",
-										 "_processInputs")				# internal functions
+										 "_checkFlags",					# internal functions
+										 "_computeBounds")
 
 # Define the class with private attributes
 @board_decorator
@@ -70,10 +67,19 @@ class Board:
 		self._x_shift_per_polygon = x_shift_per_polygon
 		self._y_shift_per_polygon = y_shift_per_polygon
 
-		# Determine if all bevel and sun information has already been preprocessed
+		# Determine if all bevel and sun information of all polygons have already been preprocessed
+		self._checkFlags()
+
+		# Compute the lower and upper x-values and y-values for the board
+		self._computeBounds()
+
+	### Define internal function for checking polygon flags and computing board bounds ###
+	def _checkFlags(self):
+		# Check the preprocess flags of each polygon and determine if they are all True
 		# Initialize the flags to True
 		self._all_bevel_info_flag = True
 		self._all_sun_info_flag = True
+
 		# Loop over the polygons and update to False if not preprocessed
 		for polygon in self._all_deepcopy_polygons:
 			polygon_info = polygon.getInfo()
@@ -82,11 +88,44 @@ class Board:
 			if polygon_info["preprocess_sun_flag"] == False:
 				self._all_sun_info_flag = False
 
+	def _computeBounds(self):
+		# Compute the render bounds for the board by looping over the polygons
+		# Initialize the bounds
+		self._x_lower = float("inf")
+		self._x_upper = -float("inf")
+		self._y_lower = float("inf")
+		self._y_upper = float("inf")
+
+		# Loop over the polygons and extract the needed information
+		for polygon in self._all_deepcopy_polygons:
+			polygon_info = polygon.getInfo()
+			self._x_lower = min(self._x_lower, polygon_info["x_lower"])
+			self._x_upper = max(self._x_upper, polygon_info["x_upper"])
+			self._y_lower = min(self._y_lower, polygon_info["y_lower"])
+			self._y_upper = max(self._y_upper, polygon_info["y_upper"])
+
 	### Define functions for preprocessing bevel and sun information for all polygons ###
-	def preprocessAllBevelInfo(self):
-		pass
+	def preprocessBevelInfo(self, bevel_attitude:Any, bevel_size:Any, polygon_index:int = None):
+		# Preprocess all information related to the bevel for all polygons (or a specific one if index is provided)
+		# Verify the polygon index (and leave the other inputs to be verified by the polygons themselves)
+		if polygon_index is not None:
+			assert type(polygon_index) == int, "Board::preprocessBevelInfo: If provided, value for 'polygon_index' must be an int object"
+			assert 0 <= polygon_index and polygon_index < self._n_polygons, "Board::preprocessBevelInfo: If provided, value for 'polygon_index' must be non-negative and less than the number of polygons on the board (i.e. " + str(self._n_polygons) + ")"
+
+		# Handle the various cases
+		if polygon_index is not None:
+			# Update the information for the given polygon
+			self._all_deepcopy_polygons[polygon_index].preprocessBevelInfo(bevel_attitude = bevel_attitude, bevel_size = bevel_size)
+		else:
+			# Update the information for all polygons
+			for polygon_index in range(self._n_polygons):
+				self._all_deepcopy_polygons[polygon_index].preprocessBevelInfo(bevel_attitude = bevel_attitude, bevel_size = bevel_size)
+
+		# Determine if all bevel and sun information of all polygons have already been preprocessed
+		self._checkFlags()
 
 	def preprocessAllSunInfo(self):
+	# Preprocess all information related to the sun for all polygons (or a specific one if index is provided)
 		pass
 
 from Polygon import SQUARE_1x1
