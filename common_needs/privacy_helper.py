e2d8af2f340e828a0f8cb3736dd3f9ebfb768f18
dralexportfolio <dr.alex.portfolio@gmail.com>
2026-04-06 11:39:53 -0700

    Update privacy_helper.py

--- common_needs/privacy_helper.py
+++ common_needs/privacy_helper.py
@@ -28,21 +28,24 @@ def privacyDecorator(attribute_names:list, deepcopy_flag:bool = True):
 
 		# Retrieve the needed magic methods from the class
 		# Always get the frist 3 magic methods
-		class_to_decorate.original__getattribute__ = class_to_decorate.__getattribute__
-		class_to_decorate.original__setattr__ = class_to_decorate.__setattr__
-		class_to_decorate.original__delattr__ = class_to_decorate.__delattr__
+		class_to_decorate.__original__getattribute__ = class_to_decorate.__getattribute__
+		class_to_decorate.__original__setattr__ = class_to_decorate.__setattr__
+		class_to_decorate.__original__delattr__ = class_to_decorate.__delattr__
 		# Only get the last one if it exists
 		if hasattr(class_to_decorate, "__getattr__") == True:
-			class_to_decorate.original__getattr__ = class_to_decorate.__getattr__
+			class_to_decorate.__original__getattr__ = class_to_decorate.__getattr__
 
 		# Define a shared function used for determining if access is allowed
-		def determineIfAccessAllowed(attribute_name:str) -> bool:
+		def _determineIfAccessAllowed(attribute_name:str) -> bool:
 			# Determine if gatekeeping is necessary for the given attribute
+			# Verify the inputs
+			assert type(attribute_name) == str, "privacyDecorator::implementPrivacy::_determineIfAccessAllowed: Provided value for 'attribute_name' must be a str object"
+
 			# Get the current frame of the program
 			current_frame = currentframe()
 
 			# End early with a True value if the caller of the operation was itself class which is being decorated
-			# Note: Need two levels of back here due to determineIfAccessAllowed being shared by the modified class methods
+			# Note: Need two levels of back here due to _determineIfAccessAllowed being shared by the modified class methods
 			calling_frame_local_variables = current_frame.f_back.f_back.f_locals
 			if "self" in calling_frame_local_variables:
 				if isinstance(calling_frame_local_variables["self"], class_to_decorate) == True:
@@ -68,45 +71,45 @@ def privacyDecorator(attribute_names:list, deepcopy_flag:bool = True):
 			return True
 
 		# Define the function implementing the modified __getattribute__ functionality
-		def modified__getattribute__(self, attribute_name:str) -> Any:
+		def __modified__getattribute__(self, attribute_name:str) -> Any:
 			# Return the original function's value if access is allowed, otherwise raise an error
 			if determineIfAccessAllowed(attribute_name = attribute_name) == True:
-				return class_to_decorate.original__getattribute__(self, attribute_name)
+				return class_to_decorate.__original__getattribute__(self, attribute_name)
 			else:
-				raise AttributeError("privacyDecorator::implementPrivacy: Unable to call __getattribute__ on the attribute named '" + attribute_name + "' due to it being private")
+				raise AttributeError("privacyDecorator::implementPrivacy::__modified__getattribute__: Unable to call '__getattribute__' on the attribute named '" + attribute_name + "' due to it being private")
 
 		# Define the function implementing the modified __getattr__ functionality
-		def modified__getattr__(self, attribute_name:str) -> Any:
+		def __modified__getattr__(self, attribute_name:str) -> Any:
 			# Return the original function's value if access is allowed, otherwise raise an error
 			if determineIfAccessAllowed(attribute_name = attribute_name) == True:
-				return class_to_decorate.original__getattr__(self, attribute_name)
+				return class_to_decorate.__original__getattr__(self, attribute_name)
 			else:
-				raise AttributeError("privacyDecorator::implementPrivacy: Unable to call __getattr__ on the attribute named '" + attribute_name + "' due to it being private")
+				raise AttributeError("privacyDecorator::implementPrivacy::__modified__getattr__: Unable to call '__getattr__' on the attribute named '" + attribute_name + "' due to it being private")
 
 		# Define the function implementing the modified __setattr__ functionality
-		def modified__setattr__(self, attribute_name:str, attribute_value:Any):
+		def __modified__setattr__(self, attribute_name:str, attribute_value:Any):
 			# Return the original function's value if access is allowed, otherwise raise an error
 			if determineIfAccessAllowed(attribute_name = attribute_name) == True:
-				return class_to_decorate.original__setattr__(self, attribute_name, attribute_value)
+				return class_to_decorate.__original__setattr__(self, attribute_name, attribute_value)
 			else:
-				raise AttributeError("privacyDecorator::implementPrivacy: Unable to call __setattr__ on the attribute named '" + attribute_name + "' due to it being private")
+				raise AttributeError("privacyDecorator::implementPrivacy::__modified__setattr__: Unable to call '__setattr__' on the attribute named '" + attribute_name + "' due to it being private")
 
 		# Define the function implementing the modified __delattr__ functionality
-		def modified__delattr__(self, attribute_name:str):
+		def __modified__delattr__(self, attribute_name:str):
 			# Return the original function's value if access is allowed, otherwise raise an error
 			if determineIfAccessAllowed(attribute_name = attribute_name) == True:
-				return class_to_decorate.original__delattr__(self, attribute_name)
+				return class_to_decorate.__original__delattr__(self, attribute_name)
 			else:
-				raise AttributeError("privacyDecorator::implementPrivacy: Unable to call __delattr__ on the attribute named '" + attribute_name + "' due to it being private")
+				raise AttributeError("privacyDecorator::implementPrivacy::__modified__delattr__: Unable to call '__delattr__' on the attribute named '" + attribute_name + "' due to it being private")
 
 		# Replace the class's original attributes with the modified ones
 		# Always set the first 3 magic methods
-		class_to_decorate.__getattribute__ = modified__getattribute__
-		class_to_decorate.__setattr__ = modified__setattr__
-		class_to_decorate.__delattr__ = modified__delattr__
+		class_to_decorate.__getattribute__ = __modified__getattribute__
+		class_to_decorate.__setattr__ = __modified__setattr__
+		class_to_decorate.__delattr__ = __modified__delattr__
 		# Only set the last one if it exists
 		if hasattr(class_to_decorate, "__getattr__") == True:
-			class_to_decorate.__getattr__ = modified__getattr__
+			class_to_decorate.__getattr__ = __modified__getattr__
 
 		# Return the modified class object
 		return class_to_decorate
